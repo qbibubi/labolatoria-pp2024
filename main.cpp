e5460fc055985d4395ccd1252f201d71c7624dff
qbibubi <58397100+qbibubi@users.noreply.github.com>
2024-10-04 18:32:49 +0200

    the website probably doesnt work for few cases

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 ﻿/*
  * Projekt posiada przykładowe rozwiązania dodatkowych zadań z labolatoriów platformy
  * "STOS" Politechnikii Gdanskiej z roku 2024/2025. Do każdego rozwiązania należy
- * wprowadzić dane ręcznie lub zawrzeć je w pliku "data.qb" według podanego przykładu
- * znajdującego się wewnątrz pliku.
+ * wprowadzić dane ręcznie.
  */
 #include "labolatorium_2/labolatorium_2.h"
 
@@ -19,17 +18,22 @@ static void CallFunction(T function, const char* functionName)
         
     function();
     
-    std::cout << std::endl;
-    std::cout << "*---------------*\n";
+    std::cout << std::endl << std::endl << std::endl;
 }
 
 int main()
 {
     // Labolatorium 2.
-    Call(Labolatorium2::HelloWorld);
-    Call(Labolatorium2::Testowe);
-    Call(Labolatorium2::SortowanieTrzechLiczb);
-    Call(Labolatorium2::WzorHerona);
+    // Call(Labolatorium2::HelloWorld);
+    // Call(Labolatorium2::Testowe);
+    // Call(Labolatorium2::SortowanieTrzechLiczb);
+    // Call(Labolatorium2::WzorHerona);
+    // Call(Labolatorium2::NierownoscTrojkata);
+    // Call(Labolatorium2::Maksimum4Liczb);
+    // Call(Labolatorium2::OdlegloscMiedzyPunktami3D);
+    // Call(Labolatorium2::PrzecinanieSieOkregow);
+    // Call(Labolatorium2::PoleTrojkataW3D);
+    Call(Labolatorium2::PierwiastkiRownaniaKwadratowego);
 
     return 0;
 }
